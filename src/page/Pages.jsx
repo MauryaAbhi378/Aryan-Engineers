c18d0d4feb3d843f06d02e9636ba33039800dbfc
MauryaAbhi378 <abhim0505@gmail.com>
2024-11-20 14:27:41 +0530

    Add Products

--- src/page/Pages.jsx
+++ src/page/Pages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from "./Home"
-import Product from "./Product"
+import Product from "./ProductList"
 
 const Pages = () => {
   return (
